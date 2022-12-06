36643d2477677f326a8f54b000ac4086330c7082
jason <lkarry0199@Gmail.com>
2022-12-06 15:04:12 +0800

    Add bug in Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,9 +1,9 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author jason
  */
 public class Collatz {
 
-    /** implementation of nextNumber! */
+    /** buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
         if (n == 1){
             return 1;
@@ -11,7 +11,7 @@ public class Collatz {
         if (n % 2 == 0){
             return n / 2;
         }
-        return 3 * n + 1;
+        return 1;
     }
 
     public static void main(String[] args) {
